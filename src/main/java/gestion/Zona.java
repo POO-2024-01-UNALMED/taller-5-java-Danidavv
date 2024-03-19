ac8f7b31778116c16fa139804b53a002ab3b4e7e
Daniel Vásquez <danvasquezmo@unal.edu.co>
2024-03-19 09:18:04 -0500

    aver

--- src/main/java/gestion/Zona.java
+++ src/main/java/gestion/Zona.java
@@ -7,49 +7,47 @@ import zooAnimales.Animal;
 public class Zona {
 	private String nombre;
 	private Zoologico zoo;
-	private ArrayList<Animal>animales= new ArrayList<>();
-
+	private ArrayList<Animal>animales = new ArrayList<>();
+	
 	public Zona() {
-		this(null,null);
+		this(null, null);
 	}
-
-	public Zona(String nombre,Zoologico zoo) {
-		this.nombre=nombre;
-		this.zoo=zoo;
-
+	
+	public Zona(String nombre, Zoologico zoo) {
+		this.nombre = nombre;
+		this.zoo = zoo;
 	}
-
-
+	
 	public void setNombre(String nombre) {
-		this.nombre=nombre;
+		this.nombre = nombre;
 	}
+	
 	public String getNombre() {
 		return nombre;
 	}
-
+	
 	public void setZoo(Zoologico zoo) {
-		this.zoo=zoo;
+		this.zoo = zoo;
 	}
+	
 	public Zoologico getZoo() {
 		return zoo;
 	}
-
+	
 	public void setAnimales(ArrayList<Animal> animales) {
-		this.animales=animales;
+		this.animales = animales;
 	}
+	
 	public  ArrayList<Animal> getAnimales() {
 		return animales;
 	}
-
-
+	
     public void agregarAnimales(Animal animal) {
     	animales.add(animal);
-
     }
+    
     public int cantidadAnimales() {
-    	int cantAnimales=animales.size();
+    	int cantAnimales = animales.size();
     	return cantAnimales;
-
     }
-
 }
